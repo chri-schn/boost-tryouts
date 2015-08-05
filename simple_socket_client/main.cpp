ecc755ac8fd70c28c5489137165fb3b980b868ce
Christian Schneider <chrischndev@gmail.com>
2015-08-05 17:24:00 +0200

    Changed simple socket to be more c++

--- simple_socket_client/main.cpp
+++ simple_socket_client/main.cpp
@@ -3,73 +3,120 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/current_function.hpp>
+#include <boost/bind.hpp>
 
 using boost::asio::ip::tcp;
 using namespace std;
 
-tcp::socket* so;
-
-std::array<char, 200> bytes;
-
-void write_handler(const boost::system::error_code &ec, std::size_t bytes_transferred);
-
-
-void read_handler(const boost::system::error_code &ec,  std::size_t bytes_transferred)
+class MySimpleClient
 {
-  cout << BOOST_CURRENT_FUNCTION << endl;
-  if (!ec)
+public:
+  MySimpleClient( boost::asio::io_service& ios, std::string host, std::string port)
+  : _ios(ios),_socket(ios),_resolver(ios),_t(ios),_host(host),_port(port)
   {
-  cout << "Empfange !!! " << bytes_transferred << " bytes: " <<int(bytes[0]) << " " << int(bytes[1]) << endl;
-  boost::asio::async_write(*so,boost::asio::buffer(bytes,2),write_handler);
+    connect();
   }
-  else
-  {
-    cout << "Error.." << ec.message() << endl;
-  }
-}
 
-void write_handler(const boost::system::error_code &ec,
-  std::size_t bytes_transferred)
-{
-  cout << BOOST_CURRENT_FUNCTION << endl;
-  if (!ec)
+private:
+  boost::asio::io_service& _ios;
+  boost::asio::ip::tcp::tcp::socket _socket;
+  tcp::resolver _resolver;
+  boost::asio::deadline_timer _t;
+  std::string _host;
+  std::string _port;
+  
+  std::vector<char> data = std::vector<char>(2);
+  
+  void reconnect_in_5_sec()
   {
-  cout << "Gesendet: " << bytes_transferred << endl;
-  boost::asio::async_read(*so,boost::asio::buffer(bytes,2),read_handler);
+    cout << BOOST_CURRENT_FUNCTION << endl;
+    _t.expires_from_now(boost::posix_time::seconds(5));
+    _t.async_wait(boost::bind(&MySimpleClient::connect,this));
+
   }
-  else
+  
+  void write_handler(const boost::system::error_code &ec, std::size_t bytes_transferred)
   {
-    cout << "Error.." << ec.message() << endl;
+    cout << BOOST_CURRENT_FUNCTION << endl;
+    if (!ec)
+    {
+    cout << "Gesendet: " << bytes_transferred << endl;
+    boost::asio::async_read(_socket,boost::asio::buffer(data,2),boost::bind(&MySimpleClient::read_handler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      cout << "Error.." << ec.message() << endl;
+      reconnect_in_5_sec();
+    }
   }
-}
+  
+  void read_handler(const boost::system::error_code &ec,  std::size_t bytes_transferred)
+  {
+    cout << BOOST_CURRENT_FUNCTION << endl;
+    if (!ec)
+    {
+    cout << "Empfange !!! " << bytes_transferred << " bytes: " <<int(data[0]) << " " << int(data[1]) << endl;
+    boost::asio::async_write(_socket,boost::asio::buffer(data,2),boost::bind(&MySimpleClient::write_handler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      cout << "Error.." << ec.message() << endl;
+      reconnect_in_5_sec();
+    }
+  }
+
+  void connect_handler(const boost::system::error_code& ec)
+  {
+    cout << BOOST_CURRENT_FUNCTION << endl;
+    if(!ec)
+    {
+        boost::asio::async_read(_socket, boost::asio::buffer(data, 2),
+        boost::bind(&MySimpleClient::read_handler, this, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+    }
+    else{
+      cout << "Error..." << ec.message() << endl;
+      reconnect_in_5_sec();
+    }
+  }
+
+  void resolve_handler(const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::iterator it)
+  {
+    cout << BOOST_CURRENT_FUNCTION << endl;
+    if(!ec)
+    {
+      boost::asio::async_connect(_socket,it,boost::bind(&MySimpleClient::connect_handler,this,boost::asio::placeholders::error));
+    }
+    else{
+      cout << "Error.." << ec.message() << endl;
+      reconnect_in_5_sec();
+    }
+  }
+  
+  void connect()
+  {
+    cout << BOOST_CURRENT_FUNCTION << endl;
+    tcp::resolver::query query(tcp::v4(),_host,_port);
+    _resolver.async_resolve(query, boost::bind(&MySimpleClient::resolve_handler, this, boost::asio::placeholders::error, boost::asio::placeholders::iterator));
+  }
+};
+
 
 int main(int argc, char* argv[])
 {
   cout << BOOST_CURRENT_FUNCTION << endl;
-
   try
   {
-    if (argc != 3)
+    if(argc != 3)
     {
-      std::cerr << "Usage: argv[0] <host> <port>\n";
-      return 1;
+      std::cerr << "Use: " << argv[0] << " <host> <port>" << std::endl;
+      return -1;
     }
-
-
-
     boost::asio::io_service io_service;
-
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query query(tcp::v4(), argv[1], argv[2]);
-    tcp::resolver::iterator iterator = resolver.resolve(query);
-
-    tcp::socket s(io_service);
-    so=&s;
-    boost::asio::connect(s, iterator);
-
-    boost::asio::async_read(s,boost::asio::buffer(bytes,2),read_handler);
-
-    io_service.run();
+    MySimpleClient msc(io_service,argv[1],argv[2]);
+    while(true){
+      io_service.run();
+      usleep(1000);
+    }
   }
   catch (std::exception& e)
   {
